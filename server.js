46cfea83545757c48619d12f97192556ac770b96
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-03-23 09:43:11 -0700

    Update server.js
    Single quotes to double quotes

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-const ApiRoutes = require('./routes/User');
+const ApiRoutes = require("./routes/User");
 
-app.use(cors())
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: false}));
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', ApiRoutes);
+app.use("/api", ApiRoutes);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
