d1e586c5cc670d8102347454981228dbb069bd25
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-03-23 09:34:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,17 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
-const API = require('./routes/User');
+const ApiRoutes = require('./routes/User');
 
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: false}));
 
-app.use('/api', API);
+app.use('/api', ApiRoutes);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
